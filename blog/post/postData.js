77d680a6ced01a3a1d87b5e21f9e339b74d5d141
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-11-18 13:53:22 +0100

    ADD: impostazione del pp[rogetto e inizializzazione dei dati

---
+++ blog/post/postData.js
@@ -0,0 +1,30 @@
+// Contenuto del file postsData.js
+const posts = [
+    {
+      title: 'Ciambellona',
+      content: 'Questo è il contenuto del primo post.',
+      image: 'images/ciambellone.jpeg',
+    },
+    {
+      title: 'Cracker alla barbabietola',
+      content: 'Questo è il contenuto del secondo post.',
+      image: 'images/cracker_barbabietola.jpeg',
+    },
+    {
+      title: 'Pane fritto dolce',
+      content: 'Questo è il contenuto del terzo post.',
+      image: 'images/pane_fritto_dolce.jpeg',
+    },
+    {
+      title: 'Pasta barbabietola',
+      content: 'Questo è il contenuto del quarto post.',
+      image: 'images/pasta_barbabietola.jpeg',
+    },
+    {
+      title: 'Torta paesana',
+      content: 'Questo è il contenuto del quinto post.',
+      image: 'images/torta_paesana.jpeg',
+    }
+  ];
+  module.exports = posts;
+  
